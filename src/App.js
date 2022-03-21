42153d0aec31a9c1deb313332fa6d84112536fa6
Jae Jun Jo <jjjo@gracefulrain.co>
2022-03-21 14:39:18 +0900

    [Mission 1] 로그인 및 메인 페이지 코드 이식

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Login from './pages/Login/Login';
+
 function App() {
-  return <div></div>;
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Login />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import './Login.css';
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login_container">
+      <div className="login_logo">JUSTGRAM</div>
+      <form className="login_form">
+        <input
+          className="login_input"
+          type="email"
+          placeholder="전화번호,사용자 이름 또는 이메일"
+        />
+        <input
+          className="login_input"
+          type="password"
+          minLength={5}
+          placeholder="비밀번호"
+        />
+        <button className="login_button">로그인</button>
+      </form>
+      <div>
+        <div className="divider"></div>
+      </div>
+      <div href="" className="forget_password">
+        비밀번호를 잊으셨나요?
+      </div>
+    </div>
+  );
 }
 
 export default Login;
